8ded5345e9e325b3c9894fea59cd3597a6998de6
Windi159 <107599426+Windi159@users.noreply.github.com>
2023-11-15 11:11:44 +0900

    Update game_select_screen.py
    게임 선택시 음악 안들리게 + ESC누르면 바탕화면으로 나와지며 노래가 다시 들리게 설정

--- 종합/game_select_screen.py
+++ 종합/game_select_screen.py
@@ -14,7 +14,9 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Want game?")
 
 #시작화면
-start_screen = pygame.image.load(".\\img\\start_screen.png")
+title_screen = ".\\img\\start_screen.png"
+
+start_screen = pygame.image.load(title_screen)
 start_screen = pygame.transform.scale(start_screen, (screen_width, screen_height))
 
 # 배경 음악 파일 경로 설정
@@ -23,7 +25,6 @@ background_music_file = ".\\music\\bgm.mp3"  # 음악 파일 경로
 # 배경 음악 초기화 및 재생
 pygame.mixer.music.load(background_music_file)
 pygame.mixer.music.set_volume(0.5)  # 볼륨 설정 (0.0부터 1.0까지)
-pygame.mixer.music.play(-1)  # -1은 무한 반복을 의미
 
 # 색상 정의
 white = (255, 255, 255)
@@ -41,26 +42,29 @@ class Button:
         self.text_color = text_color
         self.action = action
 
-    def draw(self, screen, outline=None):
+    def draw(self, main_screen, outline=None):
         if outline:
-            pygame.draw.rect(screen, outline, self.rect, 0)
-        pygame.draw.rect(screen, self.bg_color, self.rect, 0)
+            pygame.draw.rect(main_screen, outline, self.rect, 0)
+        pygame.draw.rect(main_screen, self.bg_color, self.rect, 0)
 
         if self.text:
-            font = pygame.font.Font(None, 36)
-            text_surface = font.render(self.text, True, self.text_color)
+            button_font = pygame.font.Font(None, 36)
+            text_surface = button_font.render(self.text, True, self.text_color)
             text_rect = text_surface.get_rect()
             text_rect.center = self.rect.center
-            screen.blit(text_surface, text_rect)
+            main_screen.blit(text_surface, text_rect)
 
     def is_clicked(self, got_pos):
         if self.rect.collidepoint(got_pos):
             self.action()
 
 def select_game1_action():
+    pygame.mixer.music.stop()
+    pygame.init()
     NumberYagu()
 
 def select_game2_action():
+    pygame.mixer.music.stop()
     pygame.init()
     pygame.display.set_mode((640, 200))
     pygame.display.set_caption("jump game")
@@ -68,6 +72,7 @@ def select_game2_action():
     Game_start(True)
 
 def select_game3_action():
+    pygame.mixer.music.stop()
     pygame.init()
     pygame.display.set_mode((700, 700))
     pygame.display.set_caption("omok")
@@ -75,6 +80,7 @@ def select_game3_action():
     Omok.game_start()
 
 def select_game4_action():
+    pygame.mixer.music.stop()
     pygame.init()
     pygame.display.set_mode((600, 600))
     pygame.display.set_caption("yut nori")
@@ -85,45 +91,61 @@ def quit_action():
     pygame.quit()
     sys.exit()
 
-play_game1_button = Button(200, 200, 175, 50, "NumberYagu", white, black, select_game1_action)
-play_game2_button = Button(200, 300, 175, 50, "JumpKing", white, black, select_game2_action)
-play_game3_button = Button(450, 200, 175, 50, "Omok", white, black, select_game3_action)
-play_game4_button = Button(450, 300, 175, 50, "YutNori", white, black, select_game4_action)
-quit_button = Button(325, 400, 175, 50, "End", white, black, quit_action)
+class MainScreen:
+    def __init__(self):
+        global screen
+        self.play_game1_button = Button(200, 200, 175, 50, "NumberYagu", white, black, select_game1_action)
+        self.play_game2_button = Button(200, 300, 175, 50, "JumpKing", white, black, select_game2_action)
+        self.play_game3_button = Button(450, 200, 175, 50, "Omok", white, black, select_game3_action)
+        self.play_game4_button = Button(450, 300, 175, 50, "YutNori", white, black, select_game4_action)
+        self.quit_button = Button(325, 400, 175, 50, "End", white, black, quit_action)
+        self.screen_width, self.screen_height = 1000, 620
+        self.main_screen = screen
 
-# 배경 이미지 그리기
-screen.blit(start_screen, (0, 0))
-    
-running = True
+        self.draw_screen()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                pos = pygame.mouse.get_pos()
-                if play_game1_button.is_clicked(pos):
-                    pass
+    def draw_screen(self):
+        self.screen_width, self.screen_height = 1000, 620
+        self.main_screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        pygame.display.set_caption("Want game?")
 
-                elif play_game2_button.is_clicked(pos):
-                    pass
+        self.main_screen.blit(start_screen, (0, 0))
 
-                elif play_game3_button.is_clicked(pos):
-                    pass
+        pygame.mixer.music.play(-1)
 
-                elif play_game4_button.is_clicked(pos):
-                    pass
+        running = True
 
-                elif quit_button.is_clicked(pos):
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        pos = pygame.mouse.get_pos()
+                        if self.play_game1_button.is_clicked(pos):
+                            pass
 
-   
-    play_game1_button.draw(screen, white)
-    play_game2_button.draw(screen, white)
-    play_game3_button.draw(screen, white)
-    play_game4_button.draw(screen, white)
-    quit_button.draw(screen, white)
-    pygame.display.update()
+                        elif self.play_game2_button.is_clicked(pos):
+                            pass
 
-pygame.quit()
+                        elif self.play_game3_button.is_clicked(pos):
+                            pass
+
+                        elif self.play_game4_button.is_clicked(pos):
+                            pass
+
+                        elif self.quit_button.is_clicked(pos):
+                            running = False
+
+
+            self.play_game1_button.draw(screen, white)
+            self.play_game2_button.draw(screen, white)
+            self.play_game3_button.draw(screen, white)
+            self.play_game4_button.draw(screen, white)
+            self.quit_button.draw(screen, white)
+
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    MainScreen()
